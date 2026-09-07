d1deeeb655c879615d51b11d211177160d2faa06
Λbsyllute <absyllute@gmail.com>
2026-09-07 16:48:43 +0200

    feat: basic bot login

--- src/main.py
+++ src/main.py
@@ -1,1 +1,17 @@
-print("Why did you look so far back in the commit history to find me? Get a j*b ~Absyllute")
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+CLANKER_TOKEN = getenv("BOT_TOKEN")
+
+custom_intents = discord.Intents.default()
+
+class BotClient(commands.Bot):
+    async def on_ready(self):
+        print(f"Logged in as {self.user}")
+
+client = BotClient(command_prefix="d.", intents=custom_intents)
+client.run(str(CLANKER_TOKEN))
